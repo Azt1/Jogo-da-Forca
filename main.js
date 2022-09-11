cf4113b2b4aa0593e0bfc5ffa898a138372f311c
Azt1 <inho.ilha@hotmail.com>
2022-09-11 16:55:42 -0300

    estilizaçao da telaJogo e parte do js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const palavras = [
     'abelha', 'urso', 'professor', 'macaco' , 'russo'
 ];
+
+
+function criarPalavraSecreta() {
+    const ip = parseInt(Math.random() * palavras.length)
+    
+    let PalavraSorteada;
+    PalavraSorteada = palavras[ip];
+    console.log(PalavraSorteada)
+}
+
+criarPalavraSecreta();
