88c718cc60e1fac38d78944e78807ad37d85b88d
Azt1 <inho.ilha@hotmail.com>
2022-09-14 23:47:19 -0300

    commit part js

--- main.js
+++ main.js
@@ -2,13 +2,21 @@ const palavras = [
     'abelha', 'urso', 'professor', 'macaco' , 'russo'
 ];
 
+var palavraSorteada;
+var chances = 6;
 
 function criarPalavraSecreta() {
     const ip = parseInt(Math.random() * palavras.length)
     
-    let PalavraSorteada;
-    PalavraSorteada = palavras[ip];
-    console.log(PalavraSorteada)
+    let palavraSorteada;
+    palavraSorteada = palavras[ip];
+    console.log(palavraSorteada)    
 }
 
 criarPalavraSecreta();
+
+function mostrarPalavraNaTela() {
+    const palavraTela = document.getElementsByClassName(".palavra")
+    palavraTela.innerHTML = palavraSorteada;
+}
+console.log(palavraTela)
