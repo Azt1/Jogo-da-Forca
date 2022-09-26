6f3e00010e2460bb2fad9553ef266fa7cedabc47
Azt1 <inho.ilha@hotmail.com>
2022-09-26 20:05:29 -0300

    onclick

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let chances = 6;
-let palavraSorteada
+let palavraSorteada;
 let lista = [];
 const palavras = [
     'abelha', 'urso', 'professor', 'macaco' , 'russo'
@@ -20,7 +20,7 @@ function mostrarPalavraTela() {
     const palavraTela = document.getElementsByClassName("palavra");
     palavraTela.innerHTML = "";
 
-    for(i = 0; i < palavraSorteada.length; i++) {
+    for(i = 0; i < palavraSorteada.length; i++){
         if(lista[i] == undefined){
             lista[i] = "&nbsp;"
             palavraTela.innerHTML = palavraTela.innerHTML + "<span class='letras'>" + lista[i] +"</span>"
@@ -29,4 +29,16 @@ function mostrarPalavraTela() {
             palavraTela.innerHTML = palavraTela.innerHTML + "<span class='letras'>" + lista[i] +"</span>"
         }
     }
+}
+
+function verificadorLetra(letras){
+    if(chances > 0)
+    {
+        mudarCorDaLetra("teclas-" + letras);
+    }
+}
+
+function mudarCorDaLetra(teclas){
+    document.getElementsByClassName(teclas).style.background = "#7CFC00";
+    document.getElementsByClassName(teclas).style.color = "#ffffff";
 }
