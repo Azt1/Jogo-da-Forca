9bbbce76f099dbf4f573c356a6a7861f8d918414
Azt1 <inho.ilha@hotmail.com>
2022-09-28 22:50:36 -0300

    Novo jogo da forca

--- script.js
+++ script.js
@@ -1,44 +1,100 @@
-let chances = 6;
-let palavraSorteada;
-let lista = [];
-const palavras = [
-    'abelha', 'urso', 'professor', 'macaco' , 'russo'
-];
+var programming_languages = [
+	"python",
+	"javascript",
+	"mongodb",
+	"json",
+	"java",
+	"html",
+	"css",
+	"c",
+	"csharp",
+	"golang",
+	"kotlin",
+	"php",
+	"sql",
+	"ruby"
+]
 
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
 
-criarPalavraSecreta();
-function criarPalavraSecreta() {
-    const ip = parseInt(Math.random() * palavras.length)
-    
-    let palavraSorteada;
-    palavraSorteada = palavras[ip];
-    console.log(palavraSorteada);
+function randomWord() {
+  answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
 }
 
-mostrarPalavraTela();
-function mostrarPalavraTela() {
-    const palavraTela = document.getElementsByClassName("palavra");
-    palavraTela.innerHTML = "";
+function generateButtons() {
+  let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+    `
+      <button
+        class="btn btn-lg btn-primary m-2"
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
 
-    for(i = 0; i < palavraSorteada.length; i++){
-        if(lista[i] == undefined){
-            lista[i] = "&nbsp;"
-            palavraTela.innerHTML = palavraTela.innerHTML + "<span class='letras'>" + lista[i] +"</span>"
-        }
-        else {
-            palavraTela.innerHTML = palavraTela.innerHTML + "<span class='letras'>" + lista[i] +"</span>"
-        }
-    }
+  document.getElementById('keyboard').innerHTML = buttonsHTML;
 }
 
-function verificadorLetra(letras){
-    if(chances > 0)
-    {
-        mudarCorDaLetra("teclas-" + letras);
-    }
+function handleGuess(chosenLetter) {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+  if (answer.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+    checkIfGameWon();
+  } else if (answer.indexOf(chosenLetter) === -1) {
+    mistakes++;
+    updateMistakes();
+    checkIfGameLost();
+    updateHangmanPicture();
+  }
 }
 
-function mudarCorDaLetra(teclas){
-    document.getElementsByClassName(teclas).style.background = "#7CFC00";
-    document.getElementsByClassName(teclas).style.color = "#ffffff";
+function updateHangmanPicture() {
+  document.getElementById('hangmanPic').src = './assets/trysAssets/try' + mistakes + '.svg';
 }
+
+function checkIfGameWon() {
+  if (wordStatus === answer) {
+    document.getElementById('keyboard').innerHTML = 'You Won!!!';
+  }
+}
+
+function checkIfGameLost() {
+  if (mistakes === maxWrong) {
+    document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
+    document.getElementById('keyboard').innerHTML = 'You Lost!!!';
+  }
+}
+
+function guessedWord() {
+  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+  document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+function updateMistakes() {
+  document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+function reset() {
+  mistakes = 0;
+  guessed = [];
+  document.getElementById('hangmanPic').src = './assets/trysAssets/try0.svg';
+
+  randomWord();
+  guessedWord();
+  updateMistakes();
+  generateButtons();
+}
+
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+randomWord();
+generateButtons();
+guessedWord();
